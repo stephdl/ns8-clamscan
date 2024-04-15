461ad7aa81b2b5242240576bc592d1783174c1ce
Davide Principi <davide.principi@nethesis.it>
2024-04-15 18:28:16 +0200

    tests. Add basic checks.

--- test-module.sh
+++ test-module.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2024 Nethesis S.r.l.
+# Copyright (C) 2023 Nethesis S.r.l.
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-LEADER_NODE=$1
-IMAGE_URL=$2
+#
+# Hint: access the test logs on HTTP port 8000 with this command:
+#
+#     python -mhttp.server -d tests/outputs/ 8000 &
+#
+
+set -e
+
 SSH_KEYFILE=${SSH_KEYFILE:-$HOME/.ssh/id_rsa}
 
-ssh_key="$(cat $SSH_KEYFILE)"
+LEADER_NODE="${1:?missing LEADER_NODE argument}"
+IMAGE_URL="${2:?missing IMAGE_URL argument}"
+shift 2
 
+ssh_key="$(< $SSH_KEYFILE)"
+
+cleanup() {
+    set +e
+    podman cp rf-core-runner:/home/pwuser/outputs tests/
+    podman stop rf-core-runner
+    podman rm rf-core-runner
+}
+
+trap cleanup EXIT
 podman run -i \
+    --network=host \
     -v .:/home/pwuser/ns8-module:z \
-    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:v10.0.3 \
+    --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
+    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:17.5.2 \
     bash -l -s <<EOF
-    set -e
-    echo "$ssh_key" > /home/pwuser/ns8-key
-    set -x
-    pip install -r /home/pwuser/ns8-module/tests/pythonreq.txt
-    mkdir ~/outputs
-    cd /home/pwuser/ns8-module
-    robot -v NODE_ADDR:${LEADER_NODE} \
-        -v IMAGE_URL:${IMAGE_URL} \
-        -v SSH_KEYFILE:/home/pwuser/ns8-key \
-	-d ~/outputs /home/pwuser/ns8-module/tests/
+set -e
+echo "$ssh_key" > /home/pwuser/ns8-key
+pip install -q -r /home/pwuser/ns8-module/tests/pythonreq.txt
+mkdir ~/outputs
+cd /home/pwuser/ns8-module
+exec robot -v NODE_ADDR:${LEADER_NODE} \
+    -v IMAGE_URL:${IMAGE_URL} \
+    -v SSH_KEYFILE:/home/pwuser/ns8-key \
+    --name netdata \
+    --skiponfailure unstable \
+    -d ~/outputs ${@} /home/pwuser/ns8-module/tests/
 EOF
-
-tests_res=$?
-
-podman cp rf-core-runner:/home/pwuser/outputs tests/
-podman stop rf-core-runner
-podman rm rf-core-runner
-
-exit ${tests_res}
--- tests/netdata.robot
+++ tests/netdata.robot
@@ -1,5 +1,11 @@
 *** Settings ***
 Library    SSHLibrary
+Resource   api.resource
+
+*** Variables ***
+${NETDATA_PATH}
+${NETDATA_FQDN}
+${IMAGE_URL}        ghcr.io/nethserver/netdata:latest
 
 *** Test Cases ***
 Check if netdata is installed correctly
@@ -9,7 +15,24 @@ Check if netdata is installed correctly
     &{output} =    Evaluate    ${output}
     Set Suite Variable    ${module_id}    ${output.module_id}
 
+Check netdata path is configured
+    ${ocfg} =   Run task    module/${module_id}/get-configuration    {}
+    Set Suite Variable     ${NETDATA_PATH}    ${ocfg['path']}
+    Set Suite Variable     ${NETDATA_FQDN}    ${ocfg['fqdn']}
+    Should Not Be Empty    ${NETDATA_PATH}
+    Should Not Be Empty    ${NETDATA_FQDN}
+
+Check if netdata works as expected
+    Wait Until Keyword Succeeds    20 times    3 seconds    Ping netdata
+
 Check if netdata is removed correctly
     ${rc} =    Execute Command    remove-module --no-preserve ${module_id}
     ...    return_rc=True  return_stdout=False
     Should Be Equal As Integers    ${rc}  0
+
+*** Keywords ***
+Ping netdata
+    ${out}  ${err}  ${rc} =    Execute Command    curl -k -f -H 'Host: ${NETDATA_FQDN}' https://127.0.0.1/${NETDATA_PATH}/
+    ...    return_rc=True  return_stdout=True  return_stderr=True
+    Should Be Equal As Integers    ${rc}  0
+    Should Contain    ${out}    <title>Netdata
